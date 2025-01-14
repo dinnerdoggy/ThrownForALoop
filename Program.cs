a9fdbb595418b6506df8ca637492c3d7af7dfa82
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2025-01-14 13:40:18 -0600

    User-Defined Types: Classes

--- Program.cs
+++ Program.cs
@@ -3,20 +3,33 @@ Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
 
-List<string> products = new List<string>()
+List<Product> products = new List<Product>()
 {
-    "Football",
-    "Hockey Stick",
-    "Boomerang",
-    "Frisbee",
-    "Golf Putter"
+    new Product()
+    {
+        Name = "Football",
+        Price = 15,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Hockey Stick",
+        Price = 12,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Chest Piece",
+        Price = 545,
+        Sold = false
+    }
 };
 
 Console.WriteLine("Products:");
 
 for (int i = 0; i < products.Count; i++)
 {
-    Console.WriteLine($"{i + 1}. {products[i]}");
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
 }
 
 Console.WriteLine("Please enter a product number: ");
@@ -29,4 +42,5 @@ while (response > products.Count || response < 1)
     response = int.Parse(Console.ReadLine().Trim());
 }
 
-Console.WriteLine($"You chose: {products[response - 1]}");
+Product chosenProduct = products[response - 1];
+Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not ")}sold.");
