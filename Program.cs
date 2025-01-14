bc254f736ee9624be02be651ae533319cc38634b
Casey <dinnerdoggy@gmail.com>
2025-01-13 20:56:22 -0600

    Lists

--- Program.cs
+++ Program.cs
@@ -3,20 +3,30 @@ Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
 
-Console.WriteLine(@"Products:
-1. Football
-2. Hockey Stick
-3. Boomerang
-4. Frisbee
-5. Golf Putter");
+List<string> products = new List<string>()
+{
+    "Football",
+    "Hockey Stick",
+    "Boomerang",
+    "Frisbee",
+    "Golf Putter"
+};
+
+Console.WriteLine("Products:");
+
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {products[i]}");
+}
+
 Console.WriteLine("Please enter a product number: ");
 
 int response = int.Parse(Console.ReadLine().Trim());
 
-while (response > 5 || response < 1)
+while (response > products.Count || response < 1)
 {
     Console.WriteLine("Choose a number between 1 and 5!");
     response = int.Parse(Console.ReadLine().Trim());
 }
 
-Console.WriteLine($"You chose: {response}");
+Console.WriteLine($"You chose: {products[response - 1]}");
